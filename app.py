3ecf2bd4d32abb335e2a90e0fd9731519c3c67e9
AtomAcer <simransekhon.fs@gmail.com>
2024-07-01 00:14:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -185,6 +185,7 @@ def run_chatbot(client, llm, retriever, contextualize_q_prompt, question_answer_
         chat_history = []
 
         # audio_bytes = audio_recorder()
+        audio_bytes = audio_recorder(pause_threshold=2.0, sample_rate=41_000)
         if audio_bytes:
             st.audio(audio_bytes, format="audio/wav")
 
@@ -236,7 +237,7 @@ def main():
     
     st.markdown(explanation_text)
 
-    audio_bytes = audio_recorder(pause_threshold=2.0, sample_rate=41_000)
+    # audio_bytes = audio_recorder(pause_threshold=2.0, sample_rate=41_000)
 
     client = OpenAI()
 
